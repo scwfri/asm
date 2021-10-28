93a3f5d94b47798bc1cf50fb453d5680388be9ee
scwfri <scwfri@gmail.com>
2021-10-28 17:12:08 -0500

    add gitignore.. print hello world in fizzbuzz

--- fizzbuzz.s
+++ fizzbuzz.s
@@ -2,7 +2,13 @@
 
         .text
 
+# add string to %rsi
+# add string length to %rdx
+# call printit
 main:
+        mov $hello, %rsi
+        mov $13, %rdx
+        call printit
         mov $fizz, %rsi
         mov $5, %rdx
         call printit
@@ -12,12 +18,16 @@ main:
         ret
 
 printit:
-        mov $1, %rax
-        mov $1, %rdi
+        mov $1, %rax # system call 1 => write
+        mov $1, %rdi # write to STDOUT (1)
         syscall
         ret
 
 fizz:
         .ascii "Fizz\n"
+
 buzz:
         .ascii "Buzz\n"
+
+hello:
+        .ascii "Hello World!\n"
